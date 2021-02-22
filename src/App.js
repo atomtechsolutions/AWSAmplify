f7988df0c56f715e518eca8757ebaae487bb2bcc
dt8890980 <dt8890980@gmail.com>
2021-02-22 13:36:25 +0000

    changes for hello world

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello world</h1>
       </header>
     </div>
   );
